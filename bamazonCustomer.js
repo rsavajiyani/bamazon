aad3d528413c7f11f6ef7f7ac7eb9df1c179908d
Ravi <rsavaj@gmail.com>
2018-01-30 18:22:15 -0700

    cleaned up table, added option for user to quit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql');
-const inquirer = require('inquirer'); 
-const Table = require('cli-table');
+const inquirer = require('inquirer');
+require("console.table");
+// const Table = require('cli-table');
 
 const connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: '',
-	database: 'bamazon'
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'bamazon'
 });
 
 connection.connect((err) => {
-	if (err) throw err;
+    if (err) throw err;
     console.log('connected as id ' + connection.threadId);
     displayTable();
 });
@@ -20,31 +21,17 @@ const displayTable = () => {
     let query = 'SELECT * FROM products';
     connection.query(query, (error, data) => {
         if (error) throw error;
-        console.log('Product ID || Product Name || Department Name || Price || Stock Quantity');
-        for (var i = 0; i <data.length; i++) {
-            console.log(data[i].itemID + " || " + data[i].product_name + 
-                " || " + data[i].department_name + " || " + data[i].price +
-                " || " + data[i].stock_quantity);
-        }
+        console.table(data);
         buyProduct();
-        // console.log(data[0].product_name);
-        // const table = new Table({
-        //      head: ['Product ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
-        // });
-        //  for (var i = 0; i < data.length; i++) {
-        //     table.push(data[i].product_name);
-        //  }
-        //  console.log("this is the table:\n" + table);
     });
 }
 
-const buyProduct = ()=> {
+const buyProduct = () => {
     inquirer
-        .prompt([
-            {
+        .prompt([{
                 type: "input",
                 name: "id",
-                message: "What is the ID of the product you would like to buy?",
+                message: "What is the ID of the product you would like to buy? Press Enter Twice To Exit",
             },
             {
                 type: "input",
@@ -53,19 +40,21 @@ const buyProduct = ()=> {
             }
         ]).then((input) => {
             let productID = input.id;
+            if (productID.length == 0) {
+                 process.exit();
+            }
             let unitsBought = input.units;
             let query = 'SELECT product_name, price, stock_quantity FROM products WHERE itemID=?';
             connection.query(query, [input.id], (error, data) => {
                 if (error) throw error;
-                console.log(data);
+                // console.log(data);
                 let stockQuantity = data[0].stock_quantity;
                 let price = data[0].price;
                 let productName = data[0].product_name;
                 if (unitsBought <= stockQuantity) {
-                    console.log("Purchase Complete!");
+                    console.log("Purchase Complete! You paid $" + (unitsBought * price));
                     updateDataBase(productID, unitsBought, stockQuantity, price, productName);
-                }
-                else {
+                } else {
                     console.log("Insufficient Quantity!");
                 }
             });
@@ -73,16 +62,14 @@ const buyProduct = ()=> {
 }
 
 const updateDataBase = (id, bought, stock, price, name) => {
-    console.log("running update function");
-    var query = connection.query('UPDATE products SET ? WHERE ?',
-    [
-        {
-            stock_quantity: stock - bought
-        },
-        {
-            itemID: id
-        }
-    ],
+    // console.log("running update function");
+    var query = connection.query('UPDATE products SET ? WHERE ?', [{
+                stock_quantity: stock - bought
+            },
+            {
+                itemID: id
+            }
+        ],
         function (err, res) {
             if (err) throw err;
             displayTable();
